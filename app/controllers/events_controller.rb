19e3afffd76a1916574c1fc8cfc3b2b162172850
acarlotto <acarlotto@cox.net>
2017-09-02 09:57:11 -0400

    commit before changing all

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,17 +4,16 @@ class EventsController < ApplicationController
 
   # GET /events
   def index
-    # @user_events = Event.where('user_id = ?', current_user.id)
+    # TEST @user_events = Event.where('user_id = ?', current_user.id)
     @events = Event.all
-    # @events = current_user.event(event_params)
-    # @events = current_user.events.all.order(updated_at: :desc)
 
     render json: @events
   end
 
   # GET /events/1
   def show
-    # @user_events = Event.where('user_id = ?', current_user.id)
+    # OLD @user_events = Event.find(params[:user_id])
+    # @events = Event.where(:user_id => current_user.id)
     # @events = current_user.event(event_params)
     # @user = User.find(params[:id])
     # @events = Event.find(params[:id])
@@ -22,7 +21,9 @@ class EventsController < ApplicationController
     # @events = Event.find(:set_event)
     # @events = Event.find(params[:id])
     # render json: Event.find(params[:id])
-    render json: @events
+    # render json: @events
+    # OLD render json: @user_events
+    render json: Event.find(params[:user_id])
   end
 
   # POST /events
@@ -41,8 +42,8 @@ class EventsController < ApplicationController
   # PATCH/PUT /events/1
   def update
      # @user = User.find(params[:id])
-     @event.update(event_params)
-     # @event = @user.event.find(@id)
+    @event.update(event_params)
+    # @event = @user.event.find(@id)
     # WAS JUST @event.update(event_params)
     if @event.save
     render json: @event
@@ -64,7 +65,7 @@ class EventsController < ApplicationController
 
 
     def set_user
-      # @user_id = Event.find(params[:event['user_id']])
+      @user_id = Event.find(params[:event['user_id']])
     end
 
     # Only allow a trusted parameter "white list" through.
